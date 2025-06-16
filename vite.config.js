d7509558dad1aeaa7fc73b3397285c803959d02d
Esther Xin <estherxin@Esthers-MacBook-Pro.local>
2025-06-16 15:18:10 -0400

    Changing base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/new-personal-website/',
+  base: '/',
   plugins: [react()],
   server: {
     headers: {
